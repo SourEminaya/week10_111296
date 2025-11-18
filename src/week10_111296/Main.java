c3178609327c6d3a5b2412bc88b7daaba744f9a1
W10 <nyekocharmu@gmail.com>
2025-11-18 22:45:59 +0700

    theoretically fixed

--- src/week10_111296/Main.java
+++ src/week10_111296/Main.java
@@ -13,12 +13,17 @@ public class Main {
 		int stok;
 		String warna;
 		float ppn;
-		
-		System.out.println("Menu Barang\n1. Display barang\n2. Beli barang\n3. Add barang baru");
+		boolean mainChecker=false;
+		int jumlah;
+		float total;
+		int newStok;
+		do {
+		System.out.println("Menu Barang\n1. Display barang\n2. Add barang baru\n3. Beli Barang\n4. Berhenti");
 		choice= sc.nextInt();
-		sc.hasNextLine();
+		sc.nextLine();
 		switch(choice) {
 		case 1:
+			mainChecker=true;
 			for (Barang i: list) {
 				System.out.println("Nama: "+ i.getName()+ "\nHarga: "+i.getPrice()+ "\nStok: "+i.getStock());
 				if(i instanceof Handphone) {
@@ -34,9 +39,9 @@ public class Main {
 			}
 			break;
 		case 2:
-			
+		mainChecker=true;
 		do {
-			System.out.println("Voucher/Handphone (1/2)");
+			System.out.println("Handphone/Voucher(1/2)");
 			choice=sc.nextInt();
 			sc.nextLine();
 			checker=true;
@@ -69,13 +74,104 @@ public class Main {
 			stok=sc.nextInt();
 			System.out.println("PPN: ");
 			ppn=sc.nextFloat();
-			Barang newVoucher= new Voucher(name,harga,stok,ppn);
-			
-			list.add(newVoucher);
+			Voucher newVoucher= new Voucher(name,harga,stok,ppn);
+			Barang addVoucher = newVoucher;
+			list.add(addVoucher);
 			break;
 		}
+		break;
+		case 3:
+			mainChecker=true;
+			System.out.println("Pilih barang yang dibeli (1.Voucher/2.Handphone)");
+			do {
+				System.out.println("Voucher/Handphone (1/2)");
+				choice=sc.nextInt();
+				sc.nextLine();
+				checker=true;
+				if(choice!=1) {
+					if (choice!=2) {
+						checker=false;
+					}
+				}
+			} while (checker==false);
+			int counter=0;
+			boolean counterChecker=false;
+			int beli;
+			switch(choice) {
+			case 1:
+				for (Barang i: list) {
+
+					counter++;
+					if(i instanceof Voucher) {
+						Voucher i2= (Voucher) i;
+						System.out.println(counter+"\n\n"+"Voucher: "+i2.getName()+"\nHarga: "+i2.getPrice()+"\nStok: "+i2.getStock()+"\nPPN: "+i2.getPpn());
+						counterChecker=true;
+					}
+					if (counterChecker==true) {
+					do {
+					System.out.println("Silahkan pilih index yang ingin dibeli.");
+
+					beli=sc.nextInt();
+					sc.nextLine();
+					}while ( beli<0 || beli>counter);
+					beli-=1;
+					Barang accessPoint= list.get(beli);
+					if (accessPoint instanceof Voucher) {
+						Voucher ap2=(Voucher) accessPoint;
+						do {
+						System.out.println("Stock: "+ap2.getStock()+"\nMasukkan jumlah yang mau dibeli.");
+						jumlah=sc.nextInt();
+						sc.nextLine();
+						total=(jumlah)*(1+ap2.getPpn())*ap2.getPrice();
+						}while(jumlah<0||jumlah>ap2.getStock());
+						System.out.println("Total: "+total+"rb\nJumlah yang dibeli: "+jumlah+"\nPembayaran selesai");
+						newStok=ap2.getStock()-jumlah;
+						ap2.setStock(newStok);
+						accessPoint= (Barang)ap2;
+						list.set(beli, accessPoint);
+					}else System.out.println("Barang bukanlah Voucher");
+					}else System.out.println("Tidak ada voucher di list");
+				}break;
+			case 2:
+				for (Barang i: list) {
+
+					counter++;
+					if(i instanceof Handphone) {
+						Handphone i2= (Handphone) i;
+						System.out.println(counter+"\n\n"+"Handphone: "+i2.getName()+"\nHarga: "+i2.getPrice()+"\nStok: "+i2.getStock()+"\n:Warna: "+i2.getWarna());
+						counterChecker=true;
+					}
+					if (counterChecker==true) {
+					do {
+					System.out.println("Silahkan pilih index yang ingin dibeli.");
+
+					beli=sc.nextInt();
+					sc.nextLine();
+					}while ( beli<0 || beli>counter);
+					beli-=1;
+					Barang accessPoint= list.get(beli);
+					if (accessPoint instanceof Handphone) {
+						Handphone ap2=(Handphone) accessPoint;
+						do {
+						System.out.println("Stock: "+ap2.getStock()+"\nMasukkan jumlah yang mau dibeli.");
+						jumlah=sc.nextInt();
+						sc.nextLine();
+						total=(jumlah)*ap2.getPrice();
+						}while(jumlah<0||jumlah>ap2.getStock());
+						System.out.println("Total: "+total+"rb\nJumlah yang dibeli: "+jumlah+"\nPembayaran selesai");
+						newStok=ap2.getStock()-jumlah;
+						ap2.setStock(newStok);
+						accessPoint= (Barang)ap2;
+						list.set(beli, accessPoint);
+					}else System.out.println("Barang bukanlah handphone");
+					}else System.out.println("Tidak ada handphone di list");
+				}
+			}
+			
+		
 		
 		}
+	} while (mainChecker==true);
 	}
 
 }
